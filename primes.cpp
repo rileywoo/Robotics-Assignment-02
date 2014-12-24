ad901437ee1fc0b3a7c309e982f5fcc4c8ab412a
Riley Woo <mylifeisriley@gmail.com>
2014-12-23 20:12:58 -0800

    First commit

---
+++ primes.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <cmath>
+#include <iostream>
+#define SIZE 1000000
+using namespace std;
+
+int main() {
+	ofstream primes;
+	primes.open ("primes.txt");
+	bool primesList[SIZE];
+	memset (primesList, true, sizeof(bool)*SIZE);
+	int i, j = 2;
+	for (i = 2; i < sqrt(SIZE); ++i) 
+	{
+		if (primesList[i] == true)
+		{
+			for (j = i*i; j < SIZE; j += i)
+			{
+				primesList[j] = false;
+			}
+		}
+	}
+
+	for (i = 2; i < SIZE; i++) 
+	{
+		if (primesList[i] == true) 
+		{
+			primes << i << std::endl;
+		}
+	}
+	primes.close();
+	return 0;
+}
